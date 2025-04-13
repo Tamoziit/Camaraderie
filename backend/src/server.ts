8ca492ac664fec240048d076c7f0230f4166eacd
Tamojit Das <tamojitdas181007@gmail.com>
2025-04-14 01:12:52 +0530

    Backend init

---
+++ backend/src/server.ts
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import { client } from "./redis/client";
+import connectToMongoDB from "./db/connectToMongoDB";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const corOpts = {
+    origin: '*',
+    methods: [
+        'GET',
+        'POST',
+        'PATCH',
+        'DELETE'
+    ],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization'
+    ],
+    credentials: true
+};
+
+app.use(cors(corOpts));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/api/v1', (req: Request, res: Response) => {
+    res.send('Server Up & Running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+    connectToMongoDB();
+
+    if (client) {
+        console.log("Redis connected");
+    } else {
+        console.log("Error in connecting to Redis");
+    }
+});
